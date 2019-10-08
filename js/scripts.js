197aabf5b77edc57b066eff4c2686a8ba3a7b12f
Chris Sanchez <c.sanch7@gmail.com>
2019-10-08 15:04:10 -0700

    added ability to remove li. not able to remove specific items from subTotalArray yet. added test stylization

--- js/scripts.js
+++ js/scripts.js
@@ -1,56 +1,57 @@
 //Backend Logic
-
 let taco = 1.99;
-let burrito =2.99;
-
+let burrito = 2.99
 let californiaTax = 1.07250;
 let washingtonTax = 1.065;
 let oregonTax = 1.00;
 let idahoTax = 1.06;
-
 let subtotalArray = [0];
-let subtotal = 0
-
-
+let subtotal = 0;
 // let total = subtotal  // * STATE_tax;
 
+
 //Front-End Logic
-
 $(document).ready(function () {
-
   // let menuArray = [0]
 
   // EVENTUALLY THESE WILL EACH HAVE THIER OWN IDS
-  $("#tacoButton").click(function (event) {
+  let tacoClick = $("#tacoButton").click(function (food) {
     event.preventDefault();
     // alert("Taco added!");
-    $(".items").append("<li>" + "Taco - " + taco + "</li>");
+    $(".items").append("<li><span> X </span>" + "Taco - " + taco + "</li>");
     subtotalArray.push(taco);
+    console.log("updated cart: " + subtotalArray);
+  });
+  let burittoClick = $("#burritoButton").click(function (food) {
+    event.preventDefault();
+    // alert("Burrito added!");
+    $(".items").append("<li><span> X </span>" + "Burrito - " + burrito + "</li>");
+    subtotalArray.push(burrito);
+    console.log("updated cart: " + subtotalArray);
+  });
 
-    });
+  $("ol").on("click", "li", function () {
+    console.log("Prefade: " + subtotalArray);
+    $(this).fadeOut(250);
+    console.log("Post fade: " + subtotalArray);
+    // subtotalArray.remove(this);
+    // console.log("Post remove " + subtotalArray);
 
-    $("#burritoButton").click(function (event) {
-      event.preventDefault();
-      // alert("Burrito added!");
-      $(".items").append("<li>" + "Burrito - " + burrito + "</li>");
-      subtotalArray.push(burrito);
-    });
-
-
-
-      $("#subtotalButton").click(function (event) {
-        // alert("Subtotal Requested:");
-        let subtotal = subtotalArray.reduce((a, b) => a + b);
-        $(".money").append("<li>" + "Subtotal - " + subtotal + "</li>");
-        console.log(subtotalArray);
-        // let subtotal = subtotalArray.reduce((a, b) => a + b);
-        console.log(subtotal);
-      });
+  });
 
 
+  $("#subtotalButton").click(function (event) {
+    // alert("Subtotal Requested:");
+    let subtotal = subtotalArray.reduce((a, b) => a + b).toPrecision(4);
+    $(".money").append("<li>" + "Subtotal - " + subtotal + "</li>");
+    // let subtotal = subtotalArray.reduce((a, b) => a + b);
+    console.log("Subtotal: " + subtotal);
+  });
 
 
-      // CHRIS, YES, IT MAY APPEAR like EACH OF THESE CLICK LISTENERS SHOULD BE IN-SET WITHIN EACHOTHER FOR THE SUBTOTAL FUNCTION TO WORK???
+
+
+  // CHRIS, YES, IT MAY APPEAR like EACH OF THESE CLICK LISTENERS SHOULD BE IN-SET WITHIN EACHOTHER FOR THE SUBTOTAL FUNCTION TO WORK???
 
 
 
