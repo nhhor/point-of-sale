b5aefffc54579771c9bc350991270b000d8e7903
Noah Horwitz <00noah@gmail.com>
2019-10-08 13:11:57 -0700

    add burrito test button, and subtotal test button, add working subtotal function.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,15 @@
-// https://www.sitepoint.com/jquery-loop-select-options/
-
 //Backend Logic
 
 let taco = 1.99;
+let burrito =2.99;
 
 let californiaTax = 1.07250;
 let washingtonTax = 1.065;
 let oregonTax = 1.00;
 let idahoTax = 1.06;
 
-let menuArray = [];
-
+let subtotalArray = [0];
+let subtotal = 0
 
 
 // let total = subtotal  // * STATE_tax;
@@ -19,30 +18,55 @@ let menuArray = [];
 
 $(document).ready(function () {
 
-  // let menuArray = []
+  // let menuArray = [0]
 
-  // EVENTUALLY THESE WILL TURN INTO IDS
-  $(".h4").click(function (event) {
+  // EVENTUALLY THESE WILL EACH HAVE THIER OWN IDS
+  $("#tacoButton").click(function (event) {
     event.preventDefault();
-    alert("item added!");
+    // alert("Taco added!");
     $(".items").append("<li>" + "Taco - " + taco + "</li>");
-    menuArray.push(taco)
+    subtotalArray.push(taco);
 
-    console.log(menuArray);
+    });
 
-    let subtotal = menuArray.reduce((a, b) => a + b)
-    console.log(subtotal);
-
-    // subtotalFunction(10);
-    // event.click(function () {
-    //   return "x"
-    // });
-    // let userInput = parseInt($("input#").val());
-    // let result = subtotal(userInput);
+    $("#burritoButton").click(function (event) {
+      event.preventDefault();
+      // alert("Burrito added!");
+      $(".items").append("<li>" + "Burrito - " + burrito + "</li>");
+      subtotalArray.push(burrito);
+    });
 
 
-    // $(".beepBoopResult").add();
-    // $(".beepBoopResult").append("<li style='list-style-type: none';>" + numArray + "</li>");
-  });
+
+      $("#subtotalButton").click(function (event) {
+        // alert("Subtotal Requested:");
+        let subtotal = subtotalArray.reduce((a, b) => a + b);
+        $(".money").append("<li>" + "Subtotal - " + subtotal + "</li>");
+        console.log(subtotalArray);
+        // let subtotal = subtotalArray.reduce((a, b) => a + b);
+        console.log(subtotal);
+      });
+
+
+
+
+      // CHRIS, YES, IT MAY APPEAR like EACH OF THESE CLICK LISTENERS SHOULD BE IN-SET WITHIN EACHOTHER FOR THE SUBTOTAL FUNCTION TO WORK???
+
+
+
 });
+
+// CHRIS' SKETCHPAD:
+//
+// https://www.sitepoint.com/jquery-loop-select-options/
+//
+// subtotalFunction(10);
+// event.click(function () {
+//   return "x"
 // });
+//
+// let userInput = parseInt($("input#").val());
+// let result = subtotal(userInput);
+//
+// $(".beepBoopResult").add();
+// $(".beepBoopResult").append("<li style='list-style-type: none';>" + numArray + "</li>");
