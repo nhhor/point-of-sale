0ec1ffdb7d479234062395f7c4c0f4d9f323a4cb
Student <example@example.com>
2019-10-07 20:52:38 -0700

    added click listener, added comment with looping resource

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
+// https://www.sitepoint.com/jquery-loop-select-options/
+
 //Backend Logic
 let californiaTax = 1.07250;
 let washingtonTax = 1.065;
@@ -6,28 +8,38 @@ let idahoTax = 1.06;
 let menuArray = [1,2,3];
 let subtotal = menuArray.reduce((a, b) => a + b)
 console.log(subtotal);
+
 // let total = subtotal  // * STATE_tax;
 
 //Front-End Logic
+
+
+
 $(document).ready(function () {
-    $("form#menu").submit(function (event) {
-
-        event.preventDefault();
-        // subtotalFunction(10);
-        // event.click(function () {
-        //   return "x"
-        // });
-        // let userInput = parseInt($("input#").val());
-        // let result = subtotal(userInput);
-
-        let foodOrder = []
+  // EVENTUALLY THESE WILL TURN INTO IDS
+  $(".h4").click(function (event) {
+    event.preventDefault();
+    alert("ReMix!");
 
 
 
-        // $(".beepBoopResult").add();
-        // $(".beepBoopResult").append("<li>" + "You entered: " +
-        //     userInput + "</li>");
-        // $(".beepBoopResult").append("<li style='list-style-type: none';>" + numArray + "</li>");
 
-    });
+
+    // subtotalFunction(10);
+    // event.click(function () {
+    //   return "x"
+    // });
+    // let userInput = parseInt($("input#").val());
+    // let result = subtotal(userInput);
+
+    // let foodOrder = []
+
+
+
+    // $(".beepBoopResult").add();
+    // $(".beepBoopResult").append("<li>" + "You entered: " +
+    //     userInput + "</li>");
+    // $(".beepBoopResult").append("<li style='list-style-type: none';>" + numArray + "</li>");
+  });
 });
+// });
