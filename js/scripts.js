8d0ef721361be9845aed7b7da8ae753612f253c0
Chris Sanchez <c.sanch7@gmail.com>
2019-10-08 15:59:02 -0700

    added minor script functionality and indented everything

--- js/scripts.js
+++ js/scripts.js
@@ -9,7 +9,6 @@ let subtotalArray = [0];
 let subtotal = 0;
 // let total = subtotal  // * STATE_tax;
 
-
 //Front-End Logic
 $(document).ready(function () {
   // let menuArray = [0]
@@ -18,14 +17,14 @@ $(document).ready(function () {
   let tacoClick = $("#tacoButton").click(function (food) {
     event.preventDefault();
     // alert("Taco added!");
-    $(".items").append("<li><span> X </span>" + "Taco - " + taco + "</li>");
+    $(".items").append("<li class='taco'><span> X </span>" + "Taco - " + taco + "</li>");
     subtotalArray.push(taco);
     console.log("updated cart: " + subtotalArray);
   });
   let burittoClick = $("#burritoButton").click(function (food) {
     event.preventDefault();
     // alert("Burrito added!");
-    $(".items").append("<li><span> X </span>" + "Burrito - " + burrito + "</li>");
+    $(".items").append("<li class='burrito'><span> X </span>" + "Burrito - " + burrito + "</li>");
     subtotalArray.push(burrito);
     console.log("updated cart: " + subtotalArray);
   });
@@ -35,41 +34,27 @@ $(document).ready(function () {
     $(this).parent().fadeOut(300, function () {
       console.log("Post fade: " + subtotalArray);
       $(this).remove();
-      // subtotalArray.remove(this);
-      // console.log("Post remove " + subtotalArray);
+
+
+      // subtotalArray.forEach(function(item) {
+      //   console.log(subtotalArray);
+      //   if($("li").hasClass("burrito")){
+      //     subtotalArray - 2.99;
+      //     console.log("Subtotal - burrito: " + subtotalArray)
+      //   } else if ($("li").hasClass("taco")) {
+      //     subtotalArray - 1.99;
+      //   }
+      // }); 
+      // None of this code seems to work, ideally it'd be checking to see if an element that was removed has the class of "burrito" or "taco," and if so it subtracts the hard-coded value from the array. Not entirely sure what the best option is for removing specific values from an array in a list that could be seemingly unordered. I know (subtotal - 1.99) doesn't work but I'm not sure how to make the values interact
     })
     event.stopPropagation();
   });
 
-
   $("#subtotalButton").click(function (event) {
-    // alert("Subtotal Requested:");
     let subtotal = subtotalArray.reduce((a, b) => a + b).toPrecision(4);
     $(".money").append("<li style='list-style-type: none';>" + "Subtotal - " + subtotal + "</li>");
-    // let subtotal = subtotalArray.reduce((a, b) => a + b);
     console.log("Subtotal: " + subtotal);
   });
 
-
-
-
   // CHRIS, YES, IT MAY APPEAR like EACH OF THESE CLICK LISTENERS SHOULD BE IN-SET WITHIN EACHOTHER FOR THE SUBTOTAL FUNCTION TO WORK???
-
-
-
 });
-
-// CHRIS' SKETCHPAD:
-//
-// https://www.sitepoint.com/jquery-loop-select-options/
-//
-// subtotalFunction(10);
-// event.click(function () {
-//   return "x"
-// });
-//
-// let userInput = parseInt($("input#").val());
-// let result = subtotal(userInput);
-//
-// $(".beepBoopResult").add();
-// $(".beepBoopResult").append("<li style='list-style-type: none';>" + numArray + "</li>");
